10fe1ac9391feb61a826121a1ffe7a5cbaf70fbc
Kiran Babu <kiranbabu3355@gmail.com>
2024-10-15 09:24:47 +0530

    Add files via upload

--- Table/index.php
+++ Table/index.php
@@ -1,1 +1,19 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="kiran";
 
+
+$connect=mysqli_connect($servername,$username,$password,$dbname);
+
+if($connect){
+    echo "Connected to ". $dbname . " established successfully";
+    
+}
+else{
+    die("Connection Failed: ". mysqli_connect_error());
+}
+
+
+?>
