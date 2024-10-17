d406fa8f4204a8a91c333605e4b186de146832da
Kiran Babu <kiranbabu3355@gmail.com>
2024-10-17 09:53:32 +0530

    Add files via upload

--- Table/update.php
+++ Table/update.php
@@ -1,27 +1,49 @@
-<html><?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="kiran";
-
-
-$connect=mysqli_connect($servername,$username,$password,$dbname);
+<html>
+<?php
+$connect=mysqli_connect("localhost","root","","kiran");
 
 if($connect){
-    echo "Connected to ". $dbname . " Established successfully<br>";
-    $sql="UPDATE test SET name='I love India' WHERE id = 1";
-
+    
+    $sql="Select * from test";
     $res=mysqli_query($connect,$sql);
-    if($res){
-        echo 'Record Updated Successfully!';
+
+    if(mysqli_num_rows($res)>0){
+        echo 'Select the ID to Update <br>';
+        echo '<form method="POST" action="">'; 
+        echo '<select name="selectedId">';
+        while($row=mysqli_fetch_assoc($res)){
+
+            $id=$row['id'];
+            echo '<option values="'.$id.'">'.$id. ' </option>';
+        }
+       
+        echo '</select>';
+        echo '<br><br>';
+        echo '<label for="">Enter the Text to update</label><br><br>';
+        echo '<input type="text" name="testfield" size="30"><br><br>';
+        echo '<input type="submit" name="submit" value="Update Record">';
+        echo '</form>'; 
     }
-    else{
-        die("Error: ".mysqli_error($connect));
-    }
-}
-else{
-    die("Connection Failed: ". mysqli_connect_error());
+
 }
 
+if (isset($_POST["submit"])) {
+    $selectedId = $_POST['selectedId'];
+    $newText = $_POST['testfield'];
 
-?><br><a href="/dashboard/Kiran/Table">Back</a></html>
+    
+    $updateSql = "UPDATE test SET name='$newText' WHERE id='$selectedId'"; 
+
+    if (mysqli_query($connect, $updateSql)) {
+        echo "Record updated successfully.";
+    } else {
+        echo "Error updating record: " . mysqli_error($connect);
+    }
+}
+
+mysqli_close($connect); 
+?>
+
+
+
+<br><a href="/dashboard/Kiran/Table">Back</a></html>
